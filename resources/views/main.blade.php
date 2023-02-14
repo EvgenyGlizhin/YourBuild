1b9ed3aa57a648a03f26fffa05713532433790ca
Evgeny <evgenyglizhin@gmail.com>
2023-02-14 13:53:03 +0200

    My changes in file main.blade.php and app.css

---
+++ resources/views/main.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="../css/app.css">
+
+    <title>Document</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+<header >
+    <ul class="login">
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Sign In</a></li>
+
+    </ul>
+</header>
+    <main>
+    <img class="img" src="https://vitalur.by/upload/resize_cache/iblock/d8e/1358_1358_1/d8ead480faa065c6f778feea86a2ee80.jpg" alt="">
+        <h1 class="text-h1">Your Build</h1>
+
+        <div class="div-text">
+            Вопрос строительства или ремонта рано или позно встает перед каждым человеком. Человеку далёкому от строительства
+            бывает не легко разобраться в планировании работ, технологических моментах, подщёте выборе правильного материала.
+            Наш портал поможет вам за короткое время узнать технологии выполнения различных работ, от опытных специалистов.
+            Вы сможете подщитать материалы для вашего ремонта с помощью удобных строительных калькуляторов. А ещё вы сможете
+            написать свои заметки на тему строительства и оставлять коментарии. А также, вы сможете узнать о нашей компании
+            и надежных подрядчиках с которыми мы сотрудничаем.
+
+        </div>
+</main>
+<footer>
+<div class = "socials">
+    <p class="sb gradient blue">
+        <a href="#" class="twitter">Twitter</a>
+        <a href="#" class="facebook">Facebook</a>
+        <a href="#" class="telegram">Telegram</a>
+    </p>
+</div>
+    <div>
+        <p class="text-footer">We are in socials</p>
+    </div>
+</footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () {return view('main');
 });
+
+Route::get('/welcome', function () {return view('welcome');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
