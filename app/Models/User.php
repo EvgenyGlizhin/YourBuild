9d8f4da02ae77b9738911a14401fc331124684b7
Evgeny <evgenyglizhin@gmail.com>
2023-02-15 11:52:32 +0200

    Create one-to-many relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,21 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use  HasFactory;
     use SoftDeletes;
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class, 'article_id', 'id');
+    }
+
+
 }
