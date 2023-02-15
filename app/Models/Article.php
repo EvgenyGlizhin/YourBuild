467a4d85bc6e3ff18c8ba49eb8ac21964ddca8d6
Evgeny <evgenyglizhin@gmail.com>
2023-02-15 12:48:22 +0200

    Create ArticleTag model and article_tags migration. Implement relationship many-to-many.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $table = 'articles';
+    protected $guarded = false;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'article_tags', 'article_id', 'tag_id');
+    }
 }
