c6670e7b3a4b99e78249ce019591398cfea32388
Evgeny <evgenyglizhin@gmail.com>
2023-02-17 14:01:55 +0200

    Fixed Tag

--- database/migrations/2023_02_15_085636_create_articles_table.php
+++ database/migrations/2023_02_15_085636_create_articles_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -17,10 +16,9 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('text');
-            $table->string('image_url');
-            $table->timestamp('created_at');
-            $table->timestamp('updated_at');
-            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('image_url', '2000');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
 
             $table->index('user_id', 'article_user_idx');
             $table->foreign('user_id', 'article_user_fk')->on('users')->references('id');
