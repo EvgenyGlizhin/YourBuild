8bf9684fb2cd9807720fef7caaec8bc38d151e3d
Evgeny <evgenyglizhin@gmail.com>
2023-02-24 13:39:23 +0200

    Create main, layout and include login and register

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,50 +1,36 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <script src="{{ mix('js/app.js') }}" defer></script>
-    <link rel="stylesheet" href="../css/app.css">
-
-    <title>Document</title>
-    @vite(['resources/js/app.js'])
-</head>
-<body>
-<header >
-    <ul class="login">
-        <li><a href="#">Login</a></li>
-        <li><a href="#">Sign In</a></li>
-
-    </ul>
-</header>
+@extends('layouts.layout')
+@section('content')
     <main>
-    <img class="img" src="https://vitalur.by/upload/resize_cache/iblock/d8e/1358_1358_1/d8ead480faa065c6f778feea86a2ee80.jpg" alt="">
-        <h1 class="text-h1">Your Build</h1>
-
-        <div class="div-text">
-            Вопрос строительства или ремонта рано или позно встает перед каждым человеком. Человеку далёкому от строительства
-            бывает не легко разобраться в планировании работ, технологических моментах, подщёте выборе правильного материала.
-            Наш портал поможет вам за короткое время узнать технологии выполнения различных работ, от опытных специалистов.
-            Вы сможете подщитать материалы для вашего ремонта с помощью удобных строительных калькуляторов. А ещё вы сможете
-            написать свои заметки на тему строительства и оставлять коментарии. А также, вы сможете узнать о нашей компании
-            и надежных подрядчиках с которыми мы сотрудничаем.
-
+        <div class="container">
+            <div class="mt-4 ml text-center">
+                <img width="1400px" height="170px"
+                     src="https://abrakadabra.fun/uploads/posts/2022-02/1643676064_10-abrakadabra-fun-p-fon-stroitelstvo-i-remont-13.jpg"
+                     alt="...">
+            </div>
+            <div class="row">
+                <div class="col 12 text-center p-2">
+                    <h1 class="display-3 fw-bolder">Your Build</h1>
+                </div>
+            </div>
+            <div class="col-12 text-center">
+                <p class="fs-4 fw-bold">
+                    Вопрос строительства или ремонта рано или поздно встает перед каждым человеком. Человеку далёкому от
+                    строительства
+                    бывает не легко разобраться в планировании работ, технологических моментах, подсчёте выборе
+                    правильного
+                    материала.
+                    Наш портал поможет вам за короткое время узнать технологии выполнения различных работ, от опытных
+                    специалистов.
+                    Вы сможете подсчитать материалы для вашего ремонта с помощью удобных строительных калькуляторов. А
+                    ещё
+                    вы
+                    сможете
+                    написать свои заметки на тему строительства и оставлять комментарии. А также, вы сможете узнать о
+                    нашей
+                    компании
+                    и надежных подрядчиках с которыми мы сотрудничаем.
+                </p>
+            </div>
         </div>
-</main>
-<footer>
-<div class = "socials">
-    <p class="sb gradient blue">
-        <a href="#" class="twitter">Twitter</a>
-        <a href="#" class="facebook">Facebook</a>
-        <a href="#" class="telegram">Telegram</a>
-    </p>
-</div>
-    <div>
-        <p class="text-footer">We are in socials</p>
-    </div>
-</footer>
-</body>
-</html>
+    </main>
+@endsection
